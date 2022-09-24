db569f02f7763f84af06077567ea9f2427005a4a
Bruno <brunosilvabrait@gmail.com>
2022-09-24 17:55:01 -0300

    Conteúdo extra, gems...

---
+++ Gems/gems.rb
@@ -0,0 +1,9 @@
+# gem install cpf_cnpj
+# gem install lerolero_generator
+# gem install cpf_utils
+
+require "cpf_cnpj"
+require "lerolero_generate"
+require "cpf_utils"
+
+#Instala e da um require para poder usar, os métodos contidos dentro das gems estão disponiveis dentro dos repositorios do Github.
